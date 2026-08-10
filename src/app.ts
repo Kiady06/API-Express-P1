8c3e6f792c77eb525c26616e7fc491930b5183c1
K. <148479852+Kiady06@users.noreply.github.com>
2026-08-10 22:34:04 +0300

    feat: integrate user routes into the main application

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use("/users", userRoutes);
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "API is running"
-    });
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
